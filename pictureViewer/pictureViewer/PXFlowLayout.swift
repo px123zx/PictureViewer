d1f8725e9399b7df54541682aa11f59ec0753d63
px123zx <px123zx@163.com>
2017-06-26 11:37:16 +0800

    完成图片查看器

--- pictureViewer/pictureViewer/PXFlowLayout.swift
+++ pictureViewer/pictureViewer/PXFlowLayout.swift
@@ -11,6 +11,13 @@ import UIKit
 
 class PXFlowLayout: UICollectionViewFlowLayout {
 
+    //保存前一次偏移量,用于判断滑动方向
+    var oldOffsetX: CGFloat = 0
+    //记录上一次右滑是否完成，用于处理在右滑一半在左滑的情况
+    var lastRight: Bool = true
+    
+    
+    
     override init() {
         super.init()
         self.minimumInteritemSpacing = 0
@@ -23,47 +30,47 @@ class PXFlowLayout: UICollectionViewFlowLayout {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-//    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-//        
-//       let currentItemAttributes = super.layoutAttributesForItem(at: indexPath)
-//        
-//    }
-    
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         
-        let array = super.layoutAttributesForElements(in: rect)
+        let array = NSArray(array: super.layoutAttributesForElements(in: rect)!, copyItems:true) as! [UICollectionViewLayoutAttributes]
         //可视rect
         var visibleRect = CGRect()
         visibleRect.origin = self.collectionView!.contentOffset
         visibleRect.size = self.collectionView!.bounds.size
         
+        //是否右滑
+        let isRight = self.collectionView!.contentOffset.x - oldOffsetX < 0 ? true : false
+        //缩放比例
+        var scling: CGFloat = self.collectionView!.contentOffset.x.truncatingRemainder(dividingBy: self.collectionView!.bounds.width) / collectionView!.bounds.width
         
-        
-        for (index,attributes) in array!.enumerated(){
+        for (index,attributes) in array.enumerated(){
             //cell对屏幕中心的距离
-            if index == 1 {
-                print("visibleRect = \(visibleRect.midX)")
-                print("attributes = \(attributes.center.x)")
-            }
-            
             let distance = attributes.center.x - visibleRect.midX
             
-            if distance > 0  && distance < visibleRect.width && attributes.center.x != visibleRect.width{
-                if index == 0 {
-                    print("visibleRect 0 000 = \(visibleRect.midX)")
-                    print("attributes  00000= \(attributes.center.x)")
+            if distance > 0  && distance <= visibleRect.width && index != 0{
+                //右滑动 || 右滑未完成
+                if (isRight && scling <= 0.8) || (!lastRight && scling <= 0.8){
+                    scling = 0.8
+                    lastRight = false
                 }
                 
-                //缩放比例
-                let scling: CGFloat = self.collectionView!.contentOffset.x.truncatingRemainder(dividingBy: self.collectionView!.bounds.width) / collectionView!.bounds.width
+                //右滑完成
+                if distance == visibleRect.width {
+                    lastRight = true
+                }
+                
                 
                 attributes.center = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
                 attributes.transform = attributes.transform.scaledBy(x: scling, y: scling)
                 attributes.alpha = scling
+                //调整层级关系
+                attributes.zIndex = -1
+                
+                break
             }
-            
+           
         }
+        oldOffsetX = self.collectionView!.contentOffset.x
         return array
     }
     
